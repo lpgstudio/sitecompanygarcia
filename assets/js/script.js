a81f69c3eae3848bb90c36aa508410b2a900e52b
Thiago de Paula <thiago_newdesign@hotmail.com>
2022-02-18 14:31:33 -0300

    Animação do texto

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,45 @@ function toggleMenu(){
     menuToggle.classList.toggle('active');
     menu.classList.toggle('active');
 }
+
+
+var div = document.getElementById('log');
+var textos = ['Criação de Sites', 'Gestão de Tráfego', 'Sites Personalizados', 'Social Media'];
+
+function escrever(str, done) {
+    var char = str.split('').reverse();
+    var typer = setInterval(function() {
+        if (!char.length) {
+            clearInterval(typer);
+            return setTimeout(done, 700); // só para esperar um bocadinho
+        }
+        var next = char.pop();
+        div.innerHTML += next;
+    }, 100);
+}
+
+function limpar(done) {
+    var char = div.innerHTML;
+    var nr = char.length;
+    var typer = setInterval(function() {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return done();
+        }
+        div.innerHTML = char.slice(0, nr);
+    }, 100);
+}
+
+function rodape(conteudos, el) {
+    var atual = -1;
+	function prox(cb){
+		if (atual < conteudos.length - 1) atual++;
+		else atual = 0;
+		var str = conteudos[atual];
+		escrever(str, function(){
+			limpar(prox);
+		});
+	}
+	prox(prox);
+}
+rodape(textos);
